3b55f562d1c92a3ba2a60e56deedd33b5a586602
Filip Vondrášek <filip@vondrasek.net>
2017-04-26 01:52:05 +0200

    shader drawing

--- Bengine-core/main.cpp
+++ Bengine-core/main.cpp
@@ -22,17 +22,32 @@ int main(char** argv, int argc)
 	Window window("Bengine!", 800, 600);
 	glClearColor(.2f, .3f, .8f, 1.0f);
 	
-	GLuint vao;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
+	GLuint vbo;
+
+	GLfloat vertices[] = 
+	{
+		-0.5f, -0.5f, 0.0f,
+		-0.5f,  0.5f, 0.0f,
+		 0.5f,  0.5f, 0.0f,
+		 0.5f,  0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f
+	};
+
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
 
 	Shader shader("src/shaders/basic.vert", "src/shaders/basic.frag");
+	shader.enable();
 
 	while (!window.closed())
 	{
 		window.clear();
 
-		glDrawArrays(GL_ARRAY_BUFFER, 0, 6);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
 		window.update();
 	}
